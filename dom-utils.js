d244aebbf399724a0cf80b691b1ecf147bbb5750
Amanda Chawinska <amanda.chawinska@spoko.pl>
2022-11-10 19:28:44 +0100

    added list and shot to Main

--- dom-utils.js
+++ dom-utils.js
@@ -1,4 +1,20 @@
-export const renderCountriesList = (countries) => {
-    console.log(countries);
-    // render country items into main element
+const createCountryItemElement = (country) => {
+    const countryElement = document.createElement("li");
+
+    return countryElement;
 }
+
+const createListElement = (countries) => {
+  const listElement = document.createElement("ul");
+  countries.forEach((country) => {
+    listElement.appendChild(createCountryItemElement(country));
+  });
+  return listElement;
+};
+
+export const renderCountriesList = (countries) => {
+  const rootElement = document.querySelector("#root");
+  rootElement.appendChild(createListElement(countries));
+
+  // render country items into main element
+};
