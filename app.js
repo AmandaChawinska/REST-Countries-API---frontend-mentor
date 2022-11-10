7a254a89132137cbdefc186c68bb2d9c82f13eb4
Amanda Chawinska <amanda.chawinska@spoko.pl>
2022-11-10 19:00:05 +0100

    extracting the necessary data

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const API_URL_ALL = "https://restcountries.com/v3.1/all";
 
+let countries;
+
 fetch(API_URL_ALL)
-.then((res) => res.json())
-.then((res) => console.log(res));
+  .then((res) => res.json())
+  .then((countriesRaw) => {
+    countries = countriesRaw.map((country) => {
+      return {
+        capital: country.capital && country.capital[0],
+        population: country.population,
+        name: country.name.common,
+        region: country.region,
+        flagURL: country.flags.png,
+      };
+    });
+    console.log(countries);
+  });
